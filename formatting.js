62aa0d960efddfcca41e932e96f261b75c959ad2
Oliver von Olnhausen <info@olnhausendesign.se>
2023-04-19 19:22:19 +0200

    Regex improvements

--- formatting.js
+++ formatting.js
@@ -1,18 +1,41 @@
 function format() {
-    var medications = document.getElementById('medications').value;
+    const medications = document.getElementById('medications').value;
     const results = document.getElementById('results');
 
     formatted_medications = medications
-        /* Remove spaces */
-        .replace(/\s+/g, ' ').trim()
-        /* Remove parenthesis */
-        .replace(/\(.*\)/g, '');
-
+        /* Remove extra spaces */
+        .replaceAll(/[ \t]+/g, ' ').trim()
+        /* Remove spaces at line start */
+        .replaceAll(/^\s/mg, '')
+        /* Replace Swedich chars */
+        .replaceAll(/[‰]/g, 'ä')
+        .replaceAll(/[ˆ]/g, 'ö')
+        /* Remove ... , @ */
+        .replaceAll(/[...]|,|@/g, '')
+        /* Remove HSAID */
+        .replaceAll(/ \(CS....\)/g, '')
+        .replaceAll(/ cs..../gi, '')
 
     /* Return results */
     results.innerHTML = formatted_medications;
-    console.log(formatted_medications);
+    autosize(results)
 
     /* Copy selected text into clipboard */
-    navigator.clipboard.writeText(formatted_medications.value);
+    navigator.clipboard.writeText(formatted_medications);
+}
+
+function autosize(element) {
+    element.style.height = "12rem";
+    element.style.width = "820px";
+    if (element.scrollHeight > 192) {
+        element.style.height = (element.scrollHeight + 5) + "px";
+    }
+    if (element.scrollWidth > 820) {
+        element.style.width = (element.scrollWidth + 20) + "px";
+    }
+}
+
+function reset() {
+    document.getElementById('medications').value = '';
+    document.getElementById('results').value = '';
 }
