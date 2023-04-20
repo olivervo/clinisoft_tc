4a4cb4d23de5ff70a2bf907d0f24ef5d0eea021f
Oliver von Olnhausen <info@olnhausendesign.se>
2023-04-20 17:59:42 +0200

    Format medication timing

--- formatting.js
+++ formatting.js
@@ -15,22 +15,16 @@ function format() {
         /* Remove HSAID */
         .replaceAll(/ \(CS....\)/g, '')
         .replaceAll(/ cs..../gi, '')
+        .replaceAll(/\/ 24 h/g, 'x 1')
+        .replaceAll(/\/ 12 h/g, 'x 2')
+        .replaceAll(/\/ 8 h/g, 'x 3')
+        .replaceAll(/\/ 6 h/g, 'x 4')
+        .replaceAll(/\/ 4 h/g, 'x 6')
 
     /* Return results */
     results.value = formatted_medications;
     autosize(results);
-
-    /* Animate success icon */
-    document.getElementById('success').classList.replace('h-0', 'h-6')
-    document.getElementById('success').classList.replace('w-0', 'w-6')
-    document.getElementById('success').classList.replace('opacity-0', 'opacity-1')
-
-    setTimeout(function () {
-        document.getElementById('success').classList.replace('h-6', 'h-0')
-        document.getElementById('success').classList.replace('w-6', 'w-0')
-        document.getElementById('success').classList.replace('opacity-1', 'opacity-0')
-    }, 3000)
-
+    success();
     /* Copy selected text into clipboard */
     navigator.clipboard.writeText(formatted_medications);
 }
@@ -53,4 +47,17 @@ function reset() {
 
 function copy() {
     navigator.clipboard.writeText(document.getElementById('results').value);
+}
+
+function success() {
+    /* Animate success icon */
+    document.getElementById('success').classList.replace('h-0', 'h-6')
+    document.getElementById('success').classList.replace('w-0', 'w-6')
+    document.getElementById('success').classList.replace('opacity-0', 'opacity-1')
+
+    setTimeout(function () {
+        document.getElementById('success').classList.replace('h-6', 'h-0')
+        document.getElementById('success').classList.replace('w-6', 'w-0')
+        document.getElementById('success').classList.replace('opacity-1', 'opacity-0')
+    }, 3000)
 }
